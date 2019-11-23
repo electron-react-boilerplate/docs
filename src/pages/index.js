000b3e0ed76a4dce269697ad732d5560c6be21c8
Amila Welihinda <amilajack@gmail.com>
2019-11-23 01:25:04 -0800

    add linting infra

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,3 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 import React from 'react';
 import classnames from 'classnames';
 import Layout from '@theme/Layout';
@@ -79,7 +72,7 @@ function Home() {
                 'button button--outline button--secondary button--lg',
                 styles.getStarted
               )}
-              to={useBaseUrl('docs/doc1')}
+              to={useBaseUrl('docs/installation')}
             >
               Get Started
             </Link>
@@ -91,8 +84,8 @@ function Home() {
           <section className={styles.features}>
             <div className="container">
               <div className="row">
-                {features.map((props, idx) => (
-                  <Feature key={idx} {...props} />
+                {features.map(props => (
+                  <Feature key={props.title} {...props} />
                 ))}
               </div>
             </div>
