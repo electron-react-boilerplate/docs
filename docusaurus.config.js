f322ff3344ab70f852cb209ab6c852ee3dd30c98
Amila Welihinda <amilajack@gmail.com>
2019-11-23 11:01:11 -0800

    migrate to docusarus@2 (#93)
    * bump deps
    
    * initial mvp migration
    
    * folder restucture
    
    * add linting infra
    
    * fix prose package scripts
    
    * fix netlify config
    
    * add build to test script
    
    * update footer links
    
    * misc updates to styles and setup instructions

---
+++ docusaurus.config.js
@@ -0,0 +1,109 @@
+module.exports = {
+  title: 'Electron React Boilerplate',
+  tagline: 'A Foundation for Scalable Cross-Platform Apps',
+  url: 'https://electron-react-boilerplate.js.org/',
+  baseUrl: '/',
+  favicon: 'logo/logo.png',
+  organizationName: 'electron-react-boilerplate', // Usually your GitHub org/user name.
+  projectName: 'site', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'Electron React Boilerplate',
+      logo: {
+        alt: 'ERB Logo',
+        src: 'logo/logo.png'
+      },
+      links: [
+        { to: 'docs/installation', label: 'Docs' },
+        { to: 'docs/app-showcase', label: 'Showcase' },
+        {
+          href:
+            'http://github.com/electron-react-boilerplate/examples#table-of-contents',
+          label: 'Examples'
+        },
+        {
+          href:
+            'http://github.com/electron-react-boilerplate/electron-react-boilerplate',
+          label: 'GitHub'
+        },
+        {
+          href: 'https://opencollective.com/electron-react-boilerplate-594',
+          label: 'Donate'
+        },
+        { page: 'help', label: 'Help' },
+        { blog: true, label: 'Blog' }
+      ]
+    },
+    footer: {
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Getting Started',
+              to: 'docs/installation'
+            }
+          ]
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Showcase',
+              to: 'docs/app-showcase'
+            },
+            {
+              label: 'Community Chat',
+              to: 'https://spectrum.chat/electron-react-blpt'
+            },
+            {
+              label: 'Twitter',
+              to: 'https://twitter.com/eBoilerplate'
+            },
+            {
+              label: 'Blog',
+              to: 'blog'
+            }
+          ]
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'GitHub',
+              to:
+                'https://github.com/electron-react-boilerplate/electron-react-boilerplate'
+            }
+          ]
+        }
+      ],
+      logo: {
+        alt: 'ERB Logo',
+        src: 'logo/logo.svg'
+      },
+      copyright: `Copyright © ${new Date().getFullYear()} Electron React Boilerplate`
+    },
+
+    algolia: {
+      // "Search only api key". Safe to keep this public
+      apiKey: '7989826be21123972626ebaacdc71b1a',
+      indexName: 'electron-react-boilerplate'
+    }
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js')
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css')
+        },
+        googleAnalytics: {
+          trackingID: 'UA-127797742-1'
+        }
+      }
+    ]
+  ]
+};
---
+++ src/pages/index.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import classnames from 'classnames';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import styles from './styles.module.css';
+
+const features = [
+  {
+    title: <>Faster Iteration: Hot Reloading</>,
+    description: (
+      <>
+        Make changes to your app and preview the changes without having to
+        refresh your app. Changes are made so that the state of your app is not
+        lost.
+      </>
+    )
+  },
+  {
+    title: <>Scalable: Incremental Typing</>,
+    description: (
+      <>
+        Bulding scalable apps without types can only go so far. Get type errors
+        while developing your app. Errors are thrown during compile-time and
+        runtime
+      </>
+    )
+  },
+  {
+    title: <>Performance: Build Optimizations</>,
+    description: (
+      <>
+        Optimization and minification of code with webpack comes out of the box.
+        This avoids running into perforamnce bottlenecks associated with
+        traditional electron apps
+      </>
+    )
+  }
+];
+
+function Feature({ imageUrl, title, description }) {
+  const imgUrl = useBaseUrl(imageUrl);
+  return (
+    <div className={classnames('col col--4', styles.feature)}>
+      {imgUrl && (
+        <div className="text--center">
+          <img className={styles.featureImage} src={imgUrl} alt={title} />
+        </div>
+      )}
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+function Home() {
+  const context = useDocusaurusContext();
+  const { siteConfig = {} } = context;
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />"
+    >
+      <header className={classnames('hero hero--primary', styles.heroBanner)}>
+        <div className="container">
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={styles.buttons}>
+            <Link
+              className={classnames(
+                'button button--outline button--secondary button--lg',
+                styles.getStarted
+              )}
+              to={useBaseUrl('docs/installation')}
+            >
+              Get Started
+            </Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        {features && features.length && (
+          <section className={styles.features}>
+            <div className="container">
+              <div className="row">
+                {features.map(props => (
+                  <Feature key={props.title} {...props} />
+                ))}
+              </div>
+            </div>
+          </section>
+        )}
+      </main>
+    </Layout>
+  );
+}
+
+export default Home;
