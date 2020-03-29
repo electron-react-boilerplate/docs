b1d2673328a04a55a5e6f64e361f089f46ee15b2
Dominik Stawiński <domi.stawin@gmail.com>
2020-03-29 23:11:48 +0200

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   docs: {
     'Getting Started': ['installation', 'editor-configuration'],
-    Basics: ['building', 'packaging', 'adding-assets'],
+    Basics: ['building', 'packaging', 'adding-assets', 'adding-dependencies'],
     Testing: ['component-tests', 'e2e-tests', 'ci'],
     Advanced: ['internals', 'tool-tips', 'dev-tools', 'faq'],
     Guides: ['upgrade-guide'],
